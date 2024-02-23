6dc63777fb22acf5ad128802a788ffe5fc3697a5
happy-arts <happy.arts.nft@gmail.com>
2024-02-22 20:17:00 -0600

    Updates server.js file

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const app = express();
 
 app.get('/', (req,res) => {res.send("Hello Browser")});
 
-app.get('/erick', (req,res) => {res.send("Hello Erick")});
+app.get('/erick', (req,res) => {res.send("Hello Erick Nava")});
 
 app.listen(port, ()=>console.log("Listening"));
