2e1b325a9129e40f7eb2340ecf49ab6a22354d90
mike <kaabal@protonmail.com>
2024-02-22 19:16:55 -0700

    New file Mike

--- server.js
+++ server.js
@@ -10,4 +10,5 @@ app.get('/erick', (req,res) => {res.send("Hello Erick")});
 
 app.get('/angelica', (req,res) => {res.send("Hello Angelica")}); 
 
+app.get('/Mike', (req,res) => {res.send("Hello Mike")});
 app.listen(port, ()=>console.log("Listening"));
