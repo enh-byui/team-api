62f6e07441c80ad3f472498793301ce9a99e6dac
adpsantos <san18004@byui.edu>
2024-02-29 20:08:10 -0700

    fixed the error on my file

--- test/erick.test.js
+++ test/erick.test.js
@@ -2,13 +2,11 @@ const fetch = require("node-fetch2");
 
 it("Should get a login token",async ()=>{
 
-    const response = await fetch('http://localhost:3000/angelica');
+    const response = await fetch('http://localhost:3000/erick');
     const name = await response.text();
 
     console.log(name);
 
-    expect(name).toBe("angelica");
+    expect(name).toBe("erick");
 
 })
-    
-
